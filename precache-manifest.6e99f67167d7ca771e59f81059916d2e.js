ba0d08285aeff6ae865a1d23976bb75911108a64
oliver-app-pg <oliver.app.pg@gmail.com>
2019-07-08 22:03:54 +0900

    Updates

---
+++ precache-manifest.6e99f67167d7ca771e59f81059916d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d917816b44901b788f241133bd50f6ff",
+    "url": "/pwa-test/index.html"
+  },
+  {
+    "revision": "7b7e7e520b2034f84147",
+    "url": "/pwa-test/static/css/main.01f5bfa9.chunk.css"
+  },
+  {
+    "revision": "65423ee8d3d795e33329",
+    "url": "/pwa-test/static/js/2.7f72c64f.chunk.js"
+  },
+  {
+    "revision": "7b7e7e520b2034f84147",
+    "url": "/pwa-test/static/js/main.99e146f6.chunk.js"
+  },
+  {
+    "revision": "5cf7397ead97da4fa197",
+    "url": "/pwa-test/static/js/runtime~main.1b8f209f.js"
+  }
+]);
